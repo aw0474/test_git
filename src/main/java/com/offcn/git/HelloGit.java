a8686498cbff942f209663d376f5f70eba292a7a
aw0474 <aw0474@126.com>
2022-06-15 14:43:53 +0800

    third commit

--- src/main/java/com/offcn/git/HelloGit.java
+++ src/main/java/com/offcn/git/HelloGit.java
@@ -9,5 +9,6 @@ package com.offcn.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello hot-fix!");
     }
 }

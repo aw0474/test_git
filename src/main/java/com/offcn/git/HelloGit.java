a1d02d7233472bab8a63ca239de32ef7b81477a9
aw0474 <aw0474@126.com>
2022-06-15 14:37:39 +0800

    second commit

---
+++ src/main/java/com/offcn/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.offcn.git;
+
+/**
+ * @Auther:BigData-aw
+ * @ClassName:HelloGit
+ * @功能描述:
+ * @Version:1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
